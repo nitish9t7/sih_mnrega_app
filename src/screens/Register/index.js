58d14c9fec23ed959bb330aaece0f2e6de1b07f7
aniketcodes <aniketsinha1304@gmail.com>
2022-08-22 04:31:10 +0530

    Register employee screen

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -2,11 +2,32 @@ import {StyleSheet, Text, SafeAreaView, Button} from 'react-native';
 import React, {useState, useCallback} from 'react';
 import DocumentPicker, {types} from 'react-native-document-picker';
 import {TextInput} from 'react-native';
-
+import {uploadToS3} from '../../helpers/uploadS3';
+import RNFS from 'react-native-fs';
+import axios from 'axios';
+import API from '../../constants/api';
 const Register = () => {
   const [name, setName] = useState(null);
   const [aadharNumber, setAadharNumber] = useState(null);
+  const [empId, setEmpId] = useState(null);
   const [fileResponse, setFileResponse] = useState([]);
+  const [fileData, setFileData] = useState(null);
+
+  const registerEmployee = async function () {
+    try {
+      console.log("registering")
+      const response = await axios.post(API.REGISTER_EMPLOYEE, {
+        empId,
+        name,
+        aadharNumber,
+        registrationVideoLink: `https://videotofotos.s3.us-west-2.amazonaws.com/registrationVideo/${empId}.mp4`,
+      });
+
+      console.log(response);
+    } catch (error) {
+      throw error;
+    }
+  };
 
   const handleDocumentSelection = useCallback(async () => {
     try {
@@ -14,13 +35,16 @@ const Register = () => {
         presentationStyle: 'fullScreen',
       });
       setFileResponse(response);
-      console.log(response);
+      console.log(response[0]);
+      const details = await RNFS.readFile(response[0].uri, 'base64');
+      setFileData(details);
     } catch (err) {
       console.warn(err);
     }
   }, []);
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Register New Employee</Text>
       <TextInput
         placeholder="Enter name"
         onChangeText={e => {
@@ -33,17 +57,23 @@ const Register = () => {
           setAadharNumber(e);
         }}
       />
+      <TextInput
+        placeholder="Enter Emp Id"
+        onChangeText={e => {
+          setEmpId(e);
+        }}
+      />
       <Button
         style={styles.button}
         title="Select Video 📑"
         onPress={handleDocumentSelection}
       />
-
       <Button
         title="Register"
         style={styles.button}
-        onPress={() => {
-          console.log(name, aadharNumber, fileResponse);
+        onPress={async () => {
+          await uploadToS3(fileData, `${empId}.mp4`);
+          await registerEmployee();
         }}
       />
     </SafeAreaView>
@@ -61,6 +91,6 @@ const styles = StyleSheet.create({
   },
   button: {
     margin: 20,
-    backgroundColor:"red"
+    backgroundColor: 'red',
   },
 });
